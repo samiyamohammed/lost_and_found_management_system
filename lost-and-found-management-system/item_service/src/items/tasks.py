a632e0ae84667c5cd1c7412aa589418aaae9db8d
selome <selomegech@gmail.com>
2025-01-03 12:16:27 +0300

    integration of user and item services

--- lost-and-found-management-system/item_service/src/items/tasks.py
+++ lost-and-found-management-system/item_service/src/items/tasks.py
@@ -1,7 +1,9 @@
 from celery import shared_task
+from celery.result import AsyncResult
 from .models import LostItem, FoundItem
 import pika
 import json
+import requests
 
 @shared_task
 def match_items():
@@ -36,3 +38,22 @@ def publish_message(queue_name, message):
         print(f"Error serializing message: {e}")
     finally:
         connection.close()
+
+@shared_task
+def validate_user_id(user_id):
+    try:
+        response = requests.get(f'http://127.0.0.1:8000/users/users/{user_id}/')
+        response.raise_for_status()
+        json_response = response.json()
+        print(f"JSON response: {json_response}")  # Print the JSON response
+        return json_response.get('is_valid', False)
+    except requests.HTTPError as e:
+        if e.response.status_code == 401:
+            return {'error': 'Unauthorized'}
+        elif e.response.status_code == 404:
+            return {'error': 'User not found'}
+        else:
+            return {'error': 'Validation failed'}
+    except requests.RequestException as e:
+        print(f"Error validating user ID: {e}")
+        return {'error': 'Connection error'}
